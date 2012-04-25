367d0831bfcf01b1639bacefd05a6114c12113d0
Jico Baligod <jico@baligod.com>
2012-04-25 14:17:19 -0400

    converted to yard comments

--- lib/automata/state_diagram.rb
+++ lib/automata/state_diagram.rb
@@ -1,14 +1,25 @@
 module Automata
-  
-  ##
   # A generic state diagram class represented as a 5-tuple.
-  #
-  #--
-  # TODO: Check for valid transitions against declared states and alphabet.
-  #
   class StateDiagram
     attr_accessor :states, :alphabet, :start, :accept, :transitions
     
+    # @todo Validate machine
+    #   Check that each transition reads a valid, declared
+    #   input symbol.
+    
+    # Initialize and build a StateDiagram object.
+    #
+    # @param [Hash] params the parameters to build the machine with.
+    # @option [String] :file a YAML file containing machine params.
+    # @option [Array<String>] :states the state labels of the machine.
+    # @option [Array<String>] :alphabet the alphabet of input symbols.
+    # @option [String] :start the start state of the machine.
+    # @option [Array<String>] :accept the accept states of the machine.
+    # @option [Hash] :transitions nested hash of transitions for each state.
+    # @note The :transitions hash structure will vary
+    #   Different machines will require different transition structures.
+    #   Please refer to the {http://github.com/jico/automata/wiki wiki}
+    #   for details regarding each machine type.
     def initialize(params={})
       yaml = {}
       yaml = YAML::load_file(params[:file]) if params.has_key? :file
@@ -18,7 +29,5 @@ module Automata
       @accept = yaml['accept'] || params[:accept]
       @transitions = yaml['transitions'] || params[:transitions]
     end
-    
   end
-  
 end
