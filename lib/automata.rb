d630e04d196c0dd55b0e9a7d88c83bd7092af0a2
Jico Baligod <jico@baligod.com>
2012-04-27 13:30:44 -0400

    Hash#keys_to_strings method

--- lib/automata.rb
+++ lib/automata.rb
@@ -5,5 +5,17 @@ require "automata/dfa"
 require "automata/nfa"
 
 module Automata
-  # Your code goes here...
+
+end
+
+class Hash
+  # Transforms all keys of a hash to strings.
+  #
+  # @param [Hash] the Hash whose keys to convert.
+  # @return [Hash] the new Hash with strings as keys.
+  def self.keys_to_strings(obj)
+    return obj unless obj.kind_of? Hash
+    obj = obj.inject({}){|h,(k,v)| h[k.to_s] = Hash.keys_to_strings(v); h}
+    return obj
+  end
 end
