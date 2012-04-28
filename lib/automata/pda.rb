36582aa10bb87ea25f60147f3e7842bacfb29936
Lukas Sabota <LTsmooth42@gmail.com>
2012-04-28 18:20:16 -0400

    added initial framework for pushdown automata (PDA)

---
+++ lib/automata/pda.rb
@@ -0,0 +1,14 @@
+module Automata
+  # Push Down Automata
+  class PDA < DFA
+    # Determines whether the PDA accepts a given string.
+    #
+    # This method is intended to override the parent accepts method.
+    #
+    # @param [String] input the string to use as input for PDA.
+    # @return [Boolean] whether or not hte PDA accepts the string.
+    def accepts?(input)
+      # TODO
+    end
+  end
+end
