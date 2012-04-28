c54d16b4bf6ccb8d72052e54da90cc6efbb9059b
Jico Baligod <jico@baligod.com>
2012-04-28 19:19:24 -0400

    new DFA#run method

--- lib/automata/dfa.rb
+++ lib/automata/dfa.rb
@@ -18,6 +18,26 @@ module Automata
       return true
     end
     
+    # Runs the input on the machine and returns a Hash describing
+    # the machine's final state after running.
+    #
+    # @param [String] input the string to use as input for the DFA
+    # @return [Hash] a hash describing the DFA state after running
+    #   * :input [String] the original input string
+    #   * :accept [Boolean] whether or not the DFA accepted the string
+    #   * :head [String] the state which the head is currently on
+    def run(input)
+      head = @start.to_s
+      input.each_char { |symbol| head = @transitions[head][symbol] }
+      accept = is_accept_state? head
+      resp = {
+        input: input,
+        accept: accept,
+        head: head 
+      }
+      resp
+    end
+
     # Determines whether the DFA accepts a given string.
     #
     # @param [String] input the string to use as input for the DFA.
