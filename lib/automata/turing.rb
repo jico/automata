a515571da1888c63639f4c82eec78440be3ec952
Jico Baligod <jico@baligod.com>
2012-04-29 15:33:24 -0400

    implemented the tm!

--- lib/automata/turing.rb
+++ lib/automata/turing.rb
@@ -1,7 +1,7 @@
 module Automata
   # Turing Machine
   class Turing < StateDiagram
-    attr_accessor :inputAlphabet, :tapeAlphabet
+    attr_accessor :inputAlphabet, :tapeAlphabet, :tape, :reject
 
     # @todo Check that each state is connected.
     #   Iterate through each states to verify the graph
@@ -12,50 +12,82 @@ module Automata
       yaml = YAML::load_file(params[:file]) if params.has_key? :file
       super(params)
       @inputAlphabet = params[:inputAlphabet] || yaml['inputAlphabet']
+      @tapeAlphabet = params[:tapeAlphabet] || yaml['tapeAlphabet']
+      @tape = Tape.new
+      @accept = false
+      @reject = false
     end
     
-    # Runs the input on the machine and returns a Hash describing
+    # Runs the input on the turing machine and returns a Hash describing
     # the machine's final state after running.
     #
-    # @param [String] input the string to use as input for the NFA
-    # @return [Hash] a hash describing the NFA state after running
+    # @param [String] input the string to use as input/tape for the TM
+    # @return [Hash] a hash describing the TM state after running
     #   * :input [String] the original input string
-    #   * :accept [Boolean] whether or not the NFA accepted the string
-    #   * :heads [Array] the state which the head is currently on
-    def feed(input)      
+    #   * :accept [Boolean] whether or not the TM halted in an accept state
+    #   * :reject [Boolean] whether or not the TM halted in a reject state
+    #   * :head [String] the state which the head halted
+    #   * :tape [String] the resulting tape string
+    def feed(input) 
+      @tape = Tape.new(input)
+      @accept = false
+      @reject = false
+
       stateHead = @start.to_s
-      input.each_char { |symbol| head = @transitions[head][symbol] }
-      accept = is_accept_state? head
+      input.each_char do |symbol|
+        toState = transition(stateHead, symbol)
+        if @accept || @reject
+          break
+        else
+          stateHead = toState
+        end
+      end
+      
       resp = {
         input: input,
-        accept: accept,
-        head: head 
+        accept: @accept,
+        reject: @reject,
+        head: stateHead,
+        tape: @tape.memory
       }
       resp
     end
 
-    # Determines whether the NFA accepts a given string.
+    # Determines whether the TM halts in an accept state.
     #
-    # @param [String] input the string to use as input for the NFA.
-    # @return [Boolean] Whether or not the NFA accepts the input string.
+    # @param [String] input the string to use as input for the TM.
+    # @return [Boolean] Whether or not the TM accepts the input string.
     def accepts?(input)
       resp = feed(input)
       resp[:accept]
     end
     
+    # Determines whether the TM halts in an reject state.
+    #
+    # @param [String] input the string to use as input for the TM.
+    # @return [Boolean] Whether or not the TM rejects the input string.
+    def rejects?(input)
+      resp = feed(input)
+      resp[:reject]
+    end
+
     # Determines the transition states, if any, from a given 
     # beginning state and input symbol pair.
     #
-    # @note NFA ε-transitions
-    #   ε-transitions are supported through the use of the
-    #   reserved input alphabet character '&'.
+    # @note Turing machines have two unique states:
+    #   * +ACCEPT+ Unique accept state 
+    #   * +REJECT+ Unique reject state
     # @param [String] state state label for beginning state.
     # @param [String] symbol input symbol.
     # @return [Array] Array of destination transition states.
     def transition(state, symbol)
-      dests = @transitions[state][symbol]
-      dests = [dests] unless dests.kind_of? Array
-      dests
+      actions = @transitions[state][symbol]
+      @tape.transition(symbol, actions['write'], actions['move'])
+
+      # Flip the bits for halting states
+      @accept = true if actions['to'] == 'ACCEPT'
+      @reject = true if actions['to'] == 'REJECT'
+      @head = actions['to']
     end
     
     # Determines whether or not any transition states exist
@@ -63,21 +95,55 @@ module Automata
     #
     # @param (see #transition)
     # @return [Boolean] Whether or not a transition exists.
-    def has_transition?(state, symbol)
+    def has_transition?(state, read)
       return false unless @transitions.include? state
-      @transitions[state].has_key? symbol
+      @transitions[state].has_key? read
     end
     
-    # Determines if a given state is an accept state.
-    #
-    # @param [String] state the state label to check.
-    # @return [Boolean] whether or not the state is an accept state.
-    def accept_state?(state)
-      @accept.include? state
-    end
   end
 
+  # Turing Machine tape
   class Tape
+    attr_accessor :memory
+
+    # Creates a new tape.
+    # 
+    # @param [String] string the input string
+    def initialize(string=nil)
+      if string
+        @memory = []
+        @memory << '@' unless string[0] == '@'
+        @memory += string.split('')
+        @memory << '@' unless string[-1] == '@'
+        @head = 1
+      end
+    end
+
+    # Transitions the tape.
+    #
+    # @param [String] read the input symbol read
+    # @param [String] write the symbol to write
+    # @param [String] move Either 'L', 'R', or 'S' (default) to 
+    #   move left, right, or stay, respectively.
+    # @return [Boolean] whether the transition succeeded
+    def transition(read, write, move)
+      if read == @memory[@head]
+        @memory[@head] = write
+        case move
+        when 'R'
+          # Move right
+          @memory << '@' if @memory[@head + 1]
+          @head += 1
+        when 'L'
+          # Move left
+          @memory.unshift('@') if @head == 0
+          @head -= 1
+        end
+        return true
+      else
+        return false
+      end
+    end
 
   end
 end
