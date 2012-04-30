b6a8d5c8ff35aab422dd16df0e1de0a752a473f5
Lukas Sabota <LTsmooth42@gmail.com>
2012-04-29 22:21:41 -0400

    initial feed() implementation for PDA

--- lib/automata/pda.rb
+++ lib/automata/pda.rb
@@ -12,20 +12,47 @@ module Automata
       @stack = []
     end
 
+    # Runs the input on the machine and returns a Hash describing
+    # the machine's final state after running.
+    #
+    # @param [String] input the string to use as input for the PDA
+    # @return [Hash] a hash describing the PDA state after running
+    #   * :input [String] the original input string
+    #   * :accept [Boolean] whether or not the PDA accepted the string
+    #   * :heads [Array] the state which the head is currently on
+    def feed(input)
+      head = @start
+
+      # Iterate through each symbol of input string
+      input.each_char do |symbol|
+        if has_transition?(head, symbol)
+          transition(head, symbol)
+        end
+	  end
+    
+      accept = false
+      if accept_state? head
+        accept = true
+      end
+
+      resp = {
+        input: input,
+        accept: accept,
+        head: head,
+        stack: @stack
+      }
+    end
+
+
     # Determines whether the PDA accepts a given string.
     #
     # This method is intended to override the parent accepts method.
     #
     # @param [String] input the string to use as input for PDA.
-    # @return [Boolean] whether or not hte PDA accepts the string.
+    # @return [Boolean] whether or not the PDA accepts the string.
     def accepts?(input)
       resp = feed(input)
       resp[:accept]
-    end
-
-    def has_transition?(state, symbol)
-      # TODO: test this
-      return false unless @transitions[0].include? state
     end
 
     # Determines the transition states, if any, from a given 
