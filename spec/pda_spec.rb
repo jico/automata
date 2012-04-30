c59627abfe8f43c0e3de6fea082810c72c1edc4d
Lukas Sabota <LTsmooth42@gmail.com>
2012-04-29 21:19:07 -0400

    added pda spec

---
+++ spec/pda_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+
+describe Automata::PDA do
+  
+  ##
+  # balanced bracket pda
+  #
+  context "Initializing from a valid file" do
+    before do
+      @pda = Automata::PDA.new(file: 'examples/pda_sample.yml')
+    end
+    
+    it "should accept '()'" do
+      @pda.accepts?('()').should == true
+    end
+    
+    it "should accept '(())'" do
+      @pda.accepts?('(())').should == true
+    end
+    
+    it "should not accept the empty string" do
+      @pda.accepts?('').should == false
+    end
+    
+    it "should not accept '(('" do
+      @pda.accepts?('((').should == false
+    end
+    
+    it "should accept '(())'" do
+      @pda.accepts?('10').should == true
+    end
+    
+    it "should accept '(()())'" do
+      @pda.accepts?('(()())').should == true
+    end
+    
+    it "should not accept '(()'" do
+      @pda.accepts?('110').should == false
+    end
+  end
+  
+  context "Initializing an empty pda" do
+    before do
+      @pda = Automata::PDA.new
+    end
+    
+    it "should be created successfully" do
+      @pda.should be_an_instance_of Automata::PDA
+    end
+  end
+end
